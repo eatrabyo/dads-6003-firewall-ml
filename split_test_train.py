c2eef98e1e825a4a1b7a6cb3356e7421100e1a7e
eatrabyo <itthisak.prk@gmail.com>
2023-04-13 12:40:06 +0700

    -adding stratify

--- split_test_train.py
+++ split_test_train.py
@@ -4,12 +4,12 @@ import pandas as pd
 
 fw = pd.read_csv('data_source/log2.csv')
 # split the data into train and test set
-train, test = train_test_split(fw, test_size=0.30, random_state=0)
+train, test = train_test_split(fw, test_size=0.30, random_state=0,stratify=fw['Action'])
 
 # save the data
 # train.to_csv('data_source/train.csv',index=False)
 # test.to_csv('data_source/test.csv',index=False)
 
-test = pd.read_csv('data_source/test.csv')
-train = pd.read_csv('data_source/train.csv')
+# test = pd.read_csv('data_source/test.csv')
+# train = pd.read_csv('data_source/train.csv')
 print('hello')
